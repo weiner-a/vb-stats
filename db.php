298a62c3ce1d360210c88a30da893ce594bc788f
Antek <weinerantoni@gmail.com>
2026-03-23 17:37:40 +0100

    db.php naprawione polaczenie z .env

--- db.php
+++ db.php
@@ -1,11 +1,10 @@
 <?php
 $env = parse_ini_file(__DIR__ . '/.env');
 $db = new mysqli(
-    getenv('DB_HOST'),
-    getenv('DB_USER'),
-    getenv('DB_PASS'),
-    getenv('DB_NAME'),
-    getenv('DB_PORT')
-)
-
+    $env['DB_HOST'],
+    $env['DB_USER'],
+    $env['DB_PASS'],
+    $env['DB_NAME'],
+    $env['DB_PORT']
+);
 ?>
